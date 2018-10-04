f88ca2086898c8d4bd07db3f9915febcf758b2a3
cinichka85 <cinichka85@gmail.com>
2018-10-04 10:50:52 +0300

    init commit

--- LifeCycleApp/ViewController.swift
+++ LifeCycleApp/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var lifeAppText: UITextView!
+	var isExist = false
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		lifeAppText.text = ""
+		isExist = true
 	}
-
-
 }
 
